ca152001034498250152aeace753cecbe4e83c6e
Joonas Mäkinen <makinen.jns@gmail.com>
2020-01-16 15:25:49 +0200

    Calculate lateness for station

--- dataloader.py
+++ dataloader.py
@@ -3,9 +3,14 @@ import requests
 
 
 class Dataloader:
-    def load_stations(self):
+    STATIONS_PATH = 'data/stations.pkl'
+    STATIONS_URL = 'https://rata.digitraffic.fi/api/v1/metadata/stations'
+    TRAINS_URL = 'https://rata.digitraffic.fi/api/v1/live-trains/station/'
+    TRAINS_PARAMETERS = '?arrived_trains=10&arriving_trains=0&departed_trains=10&departing_trains=0&include_nonstopping=false'
+
+    def get_and_save_stations(self):
         df = pd.DataFrame(columns=['stationName', 'stationShortCode', 'longitude', 'latitude'])
-        response = requests.get("https://rata.digitraffic.fi/api/v1/metadata/stations")
+        response = requests.get(self.STATIONS_URL)
         stations = response.json()
         for s in stations:
             df = df.append({'stationName': s['stationName'],
@@ -14,8 +19,24 @@ class Dataloader:
                             'latitude': s['latitude']},
                            ignore_index=True)
 
+        df.to_pickle(self.STATIONS_PATH)
         return df
 
-    def __init__(self):
-        locations = self.load_stations()
-        print(locations)
+    def load_stations(self):
+        df = pd.read_pickle(self.STATIONS_PATH)
+        return df
+
+    def calculate_lateness_of_station(self, station_code):
+        response = requests.get(self.TRAINS_URL + station_code + self.TRAINS_PARAMETERS)
+        trains = response.json()
+        diff = 0
+        for train in trains:
+            time_tables = train['timeTableRows']
+            for time_table in time_tables:
+                if time_table['stationShortCode'] == station_code:
+                    if 'differenceInMinutes' not in time_table:
+                        continue
+                    if time_table['differenceInMinutes'] > 0:
+                        diff += time_table['differenceInMinutes']
+        print(diff)
+        return diff
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from dataloader import Dataloader
-import pandas as pd
 
 
 class Main:
     if __name__ == "__main__":
         data_loader = Dataloader()
+        # data_loader.get_and_save_stations()
+        # df = data_loader.load_stations()
+        # print(df)
+        data_loader.calculate_lateness_of_station('SK')
